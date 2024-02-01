05371761907aa3e22cc0c68265f5db832da294d5
minaaadev <minaaadev@gmail.com>
2024-02-01 15:28:53 +0900

    list output

--- src/com/board/Article.java
+++ src/com/board/Article.java
@@ -1,20 +1,36 @@
 package com.board;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Article extends Object {
 
 	int id;
 	String title;
 	String content;
 
+	public Article(int id,String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
 	@Override 
 	//object 클래스에 있는것을 메소드 오버라이딩 한거라고 알려주기
 	public String toString() {
 		return String.format("{id : %d, title :\"%s\", content : \"%s\"}", id, title,content);
 	}
-	Article(int id, String title, String content){
-		this.id=id; 
-		this.title=title;
-		this.content=content;
+		
+	// 초기 데이터 추가를 위한 메소드
+    public static List<Article> getInitialData() {
+        List<Article> articles = new ArrayList<>();
+
+        
+
+//        for(Article article:articles) {
+//        	System.out.printf("%d / %s\n", article.id, article.title);
+//        }
+        return articles;
+		
 	}
 
 }
--- src/com/board/Main.java
+++ src/com/board/Main.java
@@ -1,14 +1,23 @@
 package com.board;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
 		int articleLastId=0;
 		
+		List<Article> articles = new ArrayList<>();		
+		
+		
+		//데이터 3개 등록
+		articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
+        
 		System.out.println("=== * 자바 텍스트 게시판 * ===");
 		System.out.println("===프로그램 시작===");
 		
@@ -27,17 +36,27 @@ public class Main {
 				int id=articleLastId+1;
 				articleLastId++;
 				
-				Article article = new Article(1,"제목","내용");
+				Article article = new Article(id, title, content);
+				articles.add(article);
 				
-				article.id = id;
-				article.title = title;
-				article.content = content;
-				
-				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
-				
+				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);				
 				System.out.printf("생성된 게시물 객체: "+article);
 			}
-			
+				else if(cmd.equals("/usr/article/list")) {
+					System.out.println("==게시물 리스트==");
+					System.out.println("번호/제목");
+					
+					for (Article article:articles) {
+						System.out.printf("%d / %s\n",article.id, article.title);
+					}
+				}
+				else if(cmd.equals("/usr/article/detail")) {
+					
+					Object lastArticle = null;
+					if(lastArticle==null) {
+						System.out.println("게시물이 존재하지 않습니다.");
+					}
+				}
 				else if (cmd.equals("exit")) {
 					System.out.println("프로그램을 종료합니다.");
 					break;
